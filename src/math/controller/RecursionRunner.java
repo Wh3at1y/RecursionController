db7fca1a558bbaf0a55093ebef07dac6412235a4
Sam Montoya <smon7958@740s-pl-118.local>
2016-02-11 13:37:04 -0700

    Added to Repo

---
+++ src/math/controller/RecursionRunner.java
@@ -0,0 +1,10 @@
+package math.controller;
+
+public class RecursionRunner
+	{
+		public static void main(String[]args)
+		{
+			RecursionController startController = new RecursionController();
+			startController.start();
+		}
+	}
---
+++ src/math/view/RecursionFrame.java
@@ -0,0 +1,28 @@
+package math.view;
+
+import javax.swing.JFrame;
+
+import math.controller.RecursionController;
+
+public class RecursionFrame extends JFrame
+	{
+		private RecursionController baseController;
+		private RecursionPanel basePanel;
+		
+		public RecursionFrame(RecursionController baseController)
+		{
+			this.baseController = baseController;
+			basePanel = new RecursionPanel(baseController);
+			
+			buildFrame();
+		}
+		
+		private void buildFrame()
+		{
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setContentPane(basePanel);
+			setSize(500,500);
+			setTitle("Recursion: (Noun), see Recursion");
+			setVisible(true);
+		}
+	}
